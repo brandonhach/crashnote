59f78f0a1b3ebeec4d992a076b45b11531785adf
brandonhach <specialcambo@gmail.com>
2024-09-08 09:54:42 -0400

    create page

--- client/src/app/create/page.tsx
+++ client/src/app/create/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+'use client';
 
-type Props = {};
+import CreateNoteForm from '@/components/CreateNoteForm';
+import dynamic from 'next/dynamic';
+import { Toaster } from 'react-hot-toast';
 
-const CreateNote = (props: Props) => {
-	return <div>CreateNote</div>;
+const Editor = dynamic(() => import('@/components/Editor'), { ssr: false });
+
+const CreateNote = () => {
+	return (
+		<div className='p-4 size-full flex flex-col overflow-x-hidden'>
+			<Toaster position='bottom-right' reverseOrder={false} />
+
+			<div className='w-full flex-grow bg-base-200/50 rounded-xl'>
+				<CreateNoteForm></CreateNoteForm>
+			</div>
+		</div>
+	);
 };
 
 export default CreateNote;
