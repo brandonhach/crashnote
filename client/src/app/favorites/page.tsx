cba5a297198e70e572caa104bf823f7cb2024bb7
brandonhach <specialcambo@gmail.com>
2024-09-09 08:56:11 -0400

    construction prop for favorites page

--- client/src/app/favorites/page.tsx
+++ client/src/app/favorites/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
 
 type Props = {};
 
 const Favorites = (props: Props) => {
-	return <div>Favorites</div>;
+	return (
+		<div className='size-full flex flex-row items-center justify-center'>
+			<div className='relative border-1 border-white size-96'>
+				<Image src={'/img/construction.png'} alt='bear' fill></Image>
+			</div>
+		</div>
+	);
 };
 
 export default Favorites;
