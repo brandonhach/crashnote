942c0cd3103a71b933e481bd5d421fb4b7788cdb
brandonhach <specialcambo@gmail.com>
2024-09-11 08:52:35 -0400

    used get req and implement data into the document

--- client/src/app/note/[id]/page.tsx
+++ client/src/app/note/[id]/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import NoteProfileCard from '@/components/NoteProfileCard';
+import { apiBaseUrl } from '@/config/api';
+import { redirect } from 'next/navigation';
 
 type Props = {};
 
-const Note = (props: Props) => {
+export async function getNoteData(id: string) {
+	const response = await fetch(`${apiBaseUrl}/api/crashnote/note/${id}`, {
+		method: 'GET',
+	});
+	if (!response.ok) {
+		redirect('/');
+	}
+	const data = await response.json();
+	return data;
+}
+
+const Note = async ({ params }: { params: { id: string } }) => {
+	const { data } = await getNoteData(params.id);
+	console.log(data);
 	const Document = dynamic(() => import('@/components/Document'), { ssr: false });
 	return (
 		<div className='p-4 size-full flex flex-row overflow-x-hidden'>
@@ -12,8 +27,8 @@ const Note = (props: Props) => {
 			<div className='w-1/6 h-full flex flex-col items-center justify-center'>
 				<NoteProfileCard></NoteProfileCard>
 			</div>
-			<div className='w-5/6 flex-grow bg-base-200/50 rounded-xl'>
-				<Document></Document>
+			<div className='w-5/6 flex-grow rounded-xl bg-[#1f1f1f]'>
+				<Document data={data}></Document>
 			</div>
 		</div>
 	);
--- client/src/components/Document.tsx
+++ client/src/components/Document.tsx
@@ -4,32 +4,30 @@ import '@blocknote/core/fonts/inter.css';
 import { useCreateBlockNote } from '@blocknote/react';
 import { BlockNoteView } from '@blocknote/mantine';
 import '@blocknote/mantine/style.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function Document() {
+interface DocumentProps {
+	data: {
+		editorContent: string;
+	};
+}
+
+export default function Document({ data }: DocumentProps) {
 	// Stores the document JSON.
 	const [blocks, setBlocks] = useState<Block[]>([]);
 
+	// Parse editorContent
+	const initialContent: Block[] = JSON.parse(data.editorContent);
+
 	// Creates a new editor instance.
 	const editor = useCreateBlockNote({
-		initialContent: [
-			{
-				type: 'paragraph',
-				content: 'Welcome to this demo!',
-			},
-			{
-				type: 'heading',
-				content: 'This is a heading block',
-			},
-			{
-				type: 'paragraph',
-				content: 'This is a paragraph block',
-			},
-			{
-				type: 'paragraph',
-			},
-		],
+		initialContent: initialContent,
 	});
+
+	// Update blocks when editor changes
+	useEffect(() => {
+		setBlocks(editor.document);
+	}, [editor]);
 
 	// Renders the editor instance and its document JSON.
 	return (
