692ac3556fa3fd765fba88bde81efd5ac344f7c3
brandonhach <specialcambo@gmail.com>
2024-09-13 08:53:40 -0400

    workaround to fixing cors policy is to use two different host which arent bind together for some reason

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,1 +1,2 @@
-export const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+export const apiGET = process.env.NEXT_PUBLIC_API_GET;
+export const apiPOST = process.env.NEXT_PUBLIC_API_POST;
